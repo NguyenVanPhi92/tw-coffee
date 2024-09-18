036aac4924a57b62ce1441d217189f3a1a63a22e
Phi Nguyen(KJ) <nguyenvanphidmx123455@gmail.com>
2024-09-18 22:58:35 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                'light-coffee': '#c89f94'
+            }
+        }
+    },
+    plugins: []
+}
